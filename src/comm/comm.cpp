be3d9140b2744c911df38dde58242d5c1f630dc6
Emilio Pomares <epomares@swaap.us>
2022-12-11 19:13:09 +0100

    Thread working

--- src/comm/comm.cpp
+++ src/comm/comm.cpp
@@ -12,16 +12,20 @@ Axioma::HorizonVM::Networking::Server::~Server()
 
 void Axioma::HorizonVM::Networking::Server::serverRun() 
 {
-    std::cout << "Starting thread";
+    std::cout << "Starting thread\n";
+    std::cout.flush();
     while(running) {
         std::this_thread::sleep_for(std::chrono::seconds(1));
-        std::cout << "One second has elapsed...";
+        std::cout << "One second has elapsed...\n";
+	std::cout.flush();
     }
 }
 
 void Axioma::HorizonVM::Networking::Server::listen(uint8_t *buffer, int maxSize)
 {
     running = true;
-    std::cout << "Wanting to start thread...";
-    std::thread t1([=] { serverRun(); });
+    std::cout << "Wanting to start thread...\n";
+    std::cout.flush();
+    std::thread t1([this] { serverRun(); });
+    t1.join();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,4 @@ int main(int argc, char *argv[])
 {
     server = new Axioma::HorizonVM::Networking::Server();
     server->listen(nullptr, 1024);
-    std::cout << "Waiting for keypress...";
-    getchar();
-    std::cout << " ... key pressed";
 }
